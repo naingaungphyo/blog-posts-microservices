fb9ef4dd66823d126ee4958500c179d1a22aeee0
naingaungphyo <5661543+naingaungphyo@users.noreply.github.com>
2020-08-18 22:16:50 +0900

    test edit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/posts', (req, res) => {
   res.send(posts)
 })
 
-app.post('/posts', async (req, res) => {
+app.post('/posts/create', async (req, res) => {
   const id = randomBytes(4).toString('hex')
   const { title } = req.body
 
@@ -40,6 +40,6 @@ app.post('/events', (req, res) => {
 })
 
 app.listen(4000, () => {
-  console.log('v100')
+  console.log('v10000')
   console.log('Listening on 4000')
 })
